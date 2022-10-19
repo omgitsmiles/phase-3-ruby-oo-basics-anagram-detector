86e34bae9aaa02110f21d0d21251553b915226be
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-10-19 10:49:06 -0400

    commit tree 4a6853263bd6d14984aacd7eb0415dc62cc218ff

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,32 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :string
+
+    def initialize(string)
+        @string = string
+    end
+
+    def match(array)
+        string_letters = string.split('')
+        array.select { |word| word.split('').sort == string_letters.sort }
+    end
+end
+
+
+
+# class Anagram
+#     attr_reader :string
+
+#     def initialize(string)
+#         @string = string
+#     end
+
+#     def match(array) 
+#         letters = @string.split('')
+#         array.select { |word| word.split('').sort == letters.sort }
+#     end
+# end
+
+# binding.pry
+# "pls"
